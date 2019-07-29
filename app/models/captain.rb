39b70766e75a39b49883d612fd29fa7474550fd3
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-29 19:02:48 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,11 +2,11 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    includes(boats: :classifications).where(classifications: {name: 'Catamaran'})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
-    includes(boats: :classifications).where(classifications: {name: 'Sailboat'})
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
   def self.motorboat_operators
@@ -14,7 +14,7 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    where("id IN (?)", self.sailors.puck(:id) & self.motorboat_operators.pluck(:id))
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
   def self.non_sailors
